7abeea601bcbbb62aac89972d860cfbf2eb31fb7
BL-Anbu <anbarasu.annadurai@bridgelabz.com>
2026-02-14 15:33:20 +0530

    UC2: Print a Hardcoded Palindrome Result

--- PlaindromeCheckerApp/src/PalindromCheckerApp.java
+++ PlaindromeCheckerApp/src/PalindromCheckerApp.java
@@ -1,6 +1,21 @@
 public class PalindromCheckerApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        String input = "madam";
+        int length = input.length();
+        boolean isPalindrome = true;
+        for (int i = 0; i < length / 2; i++) {
+            if (input.charAt(i) != input.charAt(length - 1)) {
+                System.out.println("Not a Palindrome");
+                isPalindrome = false;
+            }
+            length--;
+        }
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
     }
 }
